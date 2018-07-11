85fd6ccd2e74eafdccca7cb3c929cff19ef7ee81
berk-tepebag <berk.tepebag@gmail.com>
2018-07-11 12:55:44 +0300

    Add Polymorphism

---
+++ Polymorphism/src/Polymorphism.cpp
@@ -0,0 +1,63 @@
+//============================================================================
+// Name        : Polymorphism.cpp
+// Author      : Berk
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+class Animal{
+
+public:
+	virtual void speak(){
+
+		cout << "miyavvv" << endl;
+	}
+
+	virtual ~Animal() {};
+
+};
+
+class Cat : public Animal{
+
+public:
+	virtual void speak(){
+
+		cout << "rerere" << endl;
+	}
+
+	virtual ~Cat(){};
+
+};
+
+class StrayCat : Cat{
+
+public:
+	/*
+	* If not defined as "virtual" strayCat will call upper classes Cat.speak()
+	* "rerere" unless it is also defined "virtual". Anything not defined as
+	* "virtual" will be taken into consideration.	*
+	*/
+	virtual void speak(){
+		cout << "roooorrr" << endl;
+	}
+
+	virtual ~StrayCat(){};
+
+};
+
+
+
+int main() {
+
+	StrayCat *sCat = new StrayCat();
+
+	sCat->speak();
+
+	delete sCat;
+
+	return 0;
+}
